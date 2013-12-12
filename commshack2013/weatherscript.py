9da98a5a27d757c615e89bd49c49986519221719
Ubuntu <ubuntu@ip-10-73-145-54.eu-west-1.compute.internal>
2013-12-12 11:33:58 +0000

    few updates

--- commshack2013/weatherscript.py
+++ commshack2013/weatherscript.py
@@ -13,25 +13,29 @@ def call_weather(city):
     resp, content = h.request("http://api.openweathermap.org/data/2.5/weather?q="+city)
 
 #    print resp            
-    response=json.loads(content)
+    try:
+       response=json.loads(content)
     
-    main = response['main']
-    temp = round(main['temp'] - 273.15,2)
-    humidity = main['humidity']
-    pressure = main['pressure']
-    wind = response['wind']
-    windspeed = wind['speed']
-    winddirection = wind['deg']
-    country = response['sys']['country']
-    city = response['name']
+       main = response['main']
+       temp = round(main['temp'] - 273.15,2)
+       humidity = main['humidity']
+       pressure = main['pressure']
+       wind = response['wind']
+       windspeed = wind['speed']
+       winddirection = wind['deg']
+       country = response['sys']['country']
+       city = response['name']
 	
-    print temp, humidity, pressure, windspeed, winddirection, country, city
+       print temp, humidity, pressure, windspeed, winddirection, country, city
 
-    data = dict(temp=temp, humidity=humidity, pressure=pressure, windspeed=windspeed, winddirection=winddirection, country=country,city=city)
-    print data
-    client = mosquitto.Mosquitto("test-client")
-    print client.connect("localhost")
-    print client.publish("/weather/"+country+"/"+city, json.dumps(data), 0)
+       data = dict(temp=temp, humidity=humidity, pressure=pressure, windspeed=windspeed, winddirection=winddirection, country=country,city=city)
+       print data
+       client = mosquitto.Mosquitto("test-client")
+       print client.connect("localhost")
+       print client.publish("/weather/"+country+"/"+city, json.dumps(data), 0)
+    except Exception as inst:
+       print inst    
+    
     client.loop()
 
 while 1==1:
