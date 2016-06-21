f082749b30191d2eda1af83057639d1d451a448b
Paul Fremantle <pzfreo@github.com>
2016-06-21 15:37:39 +0100

    b

--- problem.js
+++ problem.js
@@ -1,38 +1,20 @@
 var mqtt    = require('mqtt');
-var msgpack = require("msgpack-lite");
-var client = mqtt.connect("mqtt://localhost");
-// var mongoose = require("mongoose");
 var sleep = require('sleep');
-// mongoose.connect('mongodb://localhost/test');
-var counter = 0
-// var poSchema = mongoose.Schema({
-//     date: String,
-//     id: {type: String, unique: true},
-//     lineItem: Number,
-//     quantity: Number,
-//     poNumber: String
-//    });
-// var Purchase = mongoose.model('PurchaseOrder', poSchema);
+var client = mqtt.connect("mqtt://localhost");
 
-var uuid = require('uuid');
+var counter = 0
+
 
 client.on('connect', function () {
   client.subscribe('/purchase', {qos:1});
 });
 
-// client.on('message', function (t,p) {});
 
 client.handleMessage =  function (packet, callback) {
-  // payload is msgpack buffer
   if (packet.cmd == "publish") {
     counter++;
     console.log(counter);
-    // po = new Purchase(msgpack.decode(packet.payload));
-    //console.log(msgpack.decode(packet.payload));
-    // po.save();
     sleep.usleep(100000);
   }
-
   callback();
-
 };
